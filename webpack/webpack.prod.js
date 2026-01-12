73b0bd185b5680aad678006e7bec7d85859db9b1
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2026-01-12 15:26:37 +0200

    Updated webpack config

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,5 +2,10 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'production'
+    mode: 'production',
+    performance: {
+        // Raise asset limits to avoid noise from expected vendor bundle size
+        maxEntrypointSize: 512 * 1024,
+        maxAssetSize: 512 * 1024,
+    },
 });
